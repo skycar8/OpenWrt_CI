35a13d737f8f2d4584a2a0f4017bb575908a8e4d
skycar8 <64244128+skycar8@users.noreply.github.com>
2020-04-24 17:44:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.10.10/g' ./openwrt/package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.10.10/g' ./openwrt/package/base-files/files/bin/config_generate
