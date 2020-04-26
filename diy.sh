33ea26d48faf40f565164585fd175afe69139255
skycar8 <64244128+skycar8@users.noreply.github.com>
2020-04-26 11:05:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-#1. Modify default IP
-# sed -i 's/192.168.1.1/192.168.10.10/g' ./package/base-files/files/bin/config_generate
+
+# 1.Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' ./package/base-files/files/bin/config_generate
+
+# 2.Add self packages
+"src-git Mine https://github.com/skycar8/OpenWrt-Packages.git" >> ./feeds.conf.default
