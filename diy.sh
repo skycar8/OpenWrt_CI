89ddc8077d8fdb4af37a5e556d6741f19a2cedd0
skycar8 <64244128+skycar8@users.noreply.github.com>
2020-04-26 11:11:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -4,4 +4,4 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' ./package/base-files/files/bin/config_generate
 
 # 2.Add self packages
-"src-git Mine https://github.com/skycar8/OpenWrt-Packages.git" >> ./feeds.conf.default
+echo "src-git Mine https://github.com/skycar8/OpenWrt-Packages.git" >> ./feeds.conf.default
